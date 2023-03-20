0f9f5af3742602f822df437b6a62163005608d75
jessicafiore09 <111348094+jessicafiore09@users.noreply.github.com>
2023-03-21 00:38:04 +0800

    rearranged blend and trying to beautify it...

--- src/components/BlendPage.vue
+++ src/components/BlendPage.vue
@@ -1,47 +1,37 @@
-<!-- <script>
-import Upload from './Uploads.vue'
-import InviteFriend from '../components/InviteFriend.vue'
-
-export default {
-  components: {
-    Upload, InviteFriend
-  }
-}
-</script> -->
 
 <template>
     <h1>Craft the ultimate playlist</h1>
     <h5>With your friends.</h5>
-    <h3>should we be considering the scenario where there are multiple ongoing groups? if so then maybe the first page in this section would be for them to choose the available group.</h3>
 
-
-    <!-- <div style="position: relative; top: 400px;">
-        <h5>Your Playlist</h5>
-        <div class="container-fluid">
-            <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
-                <div class="col-3">
-                    <div class="card-3 card-block"></div>
-                </div>
-                <div class="col-3">
-                    <div class="card-3 card-block"></div>
-                </div>
-                <div class="col-3">
-                    <div class="card-3 card-block"></div>
-                </div>
-                <div class="col-3">
-                    <div class="card-3 card-block"></div>
-                </div>
-                <div class="col-3">
-                    <div class="card-3 card-block"></div>
-                </div>
-                <div class="col-3">
-                    <div class="card-3 card-block"></div>
-                </div>
-                <div class="col-3">
-                    <div class="card-3 card-block"></div>
+        <div>
+            <h5>Choose Your Group</h5>
+            <div class="container-fluid">
+                <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
+                    <div v-for='item in dummy_groups' class="col-3">
+                        <div class="card-block-round card-2 text-center"></div>
+                        <div class="text-center">
+                            <h6>{{ item }}</h6>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div> -->
+
+        <div>
+            the things below should be on a separate page that takes in data dynamically
+        </div>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            dummy_groups: [
+                "group1", "group2", "group3"
+            ]
+        }
+    },
+}
+</script>
 
--- src/views/BlendView.vue
+++ src/views/BlendView.vue
@@ -1,12 +1,12 @@
 <script>
 import TopNav from '../components/TopNav.vue';
 import BlendPage from '../components/BlendPage.vue';
-import Upload from '../components/Uploads.vue';
+import Uploads from '../components/Uploads.vue';
 import InviteFriend from '../components/InviteFriend.vue'
 
 export default {
   components: {
-    TopNav, BlendPage, Upload, InviteFriend
+    TopNav, BlendPage, Uploads, InviteFriend
   }
 }
 
@@ -16,6 +16,6 @@ export default {
 <template>
     <TopNav></TopNav>
     <BlendPage></BlendPage>
-    <Upload></Upload>
+    <Uploads></Uploads>
     <InviteFriend></InviteFriend>
 </template>
