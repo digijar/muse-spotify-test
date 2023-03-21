0e66afa6f5a629b76a71eede6a4b5456469b3606
JARON CHAN <jaronchan123@gmail.com>
2023-03-21 21:13:07 +0800

    get users_top_items

--- Users_Top_Items_Module/spotify_test.py
+++ Users_Top_Items_Module/spotify_test.py
@@ -4,7 +4,7 @@
 import os
 import base64
 import requests
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, jsonify, redirect, render_template, render_template_string
 from urllib.parse import urlencode
 import json
 from flask import session
@@ -38,8 +38,23 @@ SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
 SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login-api')
+def login_api():
+    params = {
+        'client_id': SPOTIFY_CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': SPOTIFY_REDIRECT_URI,
+        'scope': 'user-read-private user-read-email user-top-read',
+    }
+    auth_url = f'{SPOTIFY_AUTH_URL}?{urlencode(params)}'
+    return jsonify({"auth_url": auth_url})
 
 @app.route('/login')
 def login():
@@ -56,6 +71,7 @@ def login():
 def callback():
     code = request.args.get('code')
     if code:
+        # Code for exchanging the authorization code for an access token
         token_data = {
             'grant_type': 'authorization_code',
             'code': code,
@@ -68,7 +84,6 @@ def callback():
         if response.status_code == 200:
             tokens = response.json()
             session['access_token'] = tokens['access_token']
-            session['refresh_token'] = tokens['refresh_token']
             return redirect('/top_artists_tracks')
         else:
             return jsonify({"code": response.status_code, "message": "An error occurred while exchanging the authorization code for an access token."}), response.status_code
@@ -78,13 +93,31 @@ def callback():
 
 @app.route('/top_artists_tracks')
 def top_artists_tracks():
-    headers = {
-        'Authorization': f"Bearer {session['access_token']}"
-    }
-    top_artists = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers).json()
-    top_tracks = requests.get('https://api.spotify.com/v1/me/top/tracks', headers=headers).json()
+    access_token = request.headers.get('Authorization', '').replace('Bearer ', '')
+    if not access_token and 'access_token' in session:
+        access_token = session['access_token']
 
-    return jsonify({"top_artists": top_artists, "top_tracks": top_tracks})
+    if access_token:
+        return render_template("top_artists_tracks.html", access_token=access_token)
+    else:
+        return jsonify({"code": 400, "message": "Bad request. Missing access token."}), 400
+    
+@app.route('/top_artists_tracks_data')
+def top_artists_tracks_data():
+    access_token = request.headers.get('Authorization', '').replace('Bearer ', '')
+    if not access_token and 'access_token' in session:
+        access_token = session['access_token']
+
+    if access_token:
+        headers = {
+            'Authorization': f"Bearer {access_token}"
+        }
+        top_artists = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers).json()
+        top_tracks = requests.get('https://api.spotify.com/v1/me/top/tracks', headers=headers).json()
+
+        return jsonify({"top_artists": top_artists["items"], "top_tracks": top_tracks["items"]})
+    else:
+        return jsonify({"code": 400, "message": "Bad request. Missing access token."}), 400
 
 if __name__ == '__main__':
     print("This is flask for " + os.path.basename(__file__) + ": manage Spotify Authorization Code Flow ...")
