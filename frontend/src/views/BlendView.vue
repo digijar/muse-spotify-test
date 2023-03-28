a2cd98db55cf15602f30c26e3a311c5349cf29de
JARON CHAN <jaronchan123@gmail.com>
2023-03-28 12:50:02 +0800

    Update BlendView.vue

--- frontend/src/views/BlendView.vue
+++ frontend/src/views/BlendView.vue
@@ -16,7 +16,12 @@ export default {
 <template>
   <div>
     <TopNav></TopNav>
+  </div>
+  <div>
+    <br><br><br><br><br>
+    <h4 style="text-align: center;">Group: {{ $route.params.group_name }}</h4>
     <Uploads></Uploads>
     <InviteFriend></InviteFriend>
+    <br><br><br><br>
   </div>
 </template>
