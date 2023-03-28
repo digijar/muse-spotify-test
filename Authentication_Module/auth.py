c4c8043bb383f590034d8090872df30869ee5b1e
JARON CHAN <jaronchan123@gmail.com>
2023-03-28 08:52:39 +0800

    changed just abit

--- Authentication_Module/auth.py
+++ Authentication_Module/auth.py
@@ -35,8 +35,5 @@ def create_user():
     except :
         return jsonify({'success': False})
 
-    
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)
